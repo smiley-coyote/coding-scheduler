ec051e37ba27705c119aa7b5d77d80f2d83131bd
smiley-coyote <nick.a.alex@gmail.com>
2019-01-17 11:12:48 -0500

    basic functions started

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,80 @@
 var toDo =[
   {
-    item: "HTML/CSS/JavaScript",
-    selected: false
+    item: "HTML/CSS/JavaScript"
   },
   {
-    item:"Node/SQL",
-    selected: false
+    item:"Node/SQL"
   },
   {
-    item:"React/Mongo",
-    selected: false
+    item:"React/Mongo"
   },
   {
-    item:"Software/Mobile Day",
-    selected: false
+    item:"Software/Mobile Day"
   },
   {
-    item:"Learn something new or catch up on something old",
-    selected: false
+    item:"Learn something new or catch up on something old"
   }
 ];
-var myWeek = [
-  {
-    day: "Monday",
-    work: "",
-    item: ""
-  },
-  {
-    day: "Tuesday",
-    work: "",
-    item: ""
-  },
-  {
-    day: "Wednesday",
-    work: "",
-    item: ""
-  },
-  {
-    day: "Thursday",
-    work: "",
-    item: ""
-  },
-  {
-    day: "Friday",
-    work: "",
-    item: ""
-  },
-  {
-    day: "Saturday",
-    work: "",
-    item: ""
-  },
-  {
-    day: "Sunday",
-    work: "",
-    item: ""
-  }
-]
+
+var myWeek = [];
+
 
 function scheduler(){
-  
+  event.preventDefault();
+  myWeek = [
+    {
+      day: "Monday",
+      work: $("#monday").val(),
+      item: ""
+    },
+    {
+      day: "Tuesday",
+      work: $("#tuesday").val(),
+      item: ""
+    },
+    {
+      day: "Wednesday",
+      work: $("#wednesday").val(),
+      item: ""
+    },
+    {
+      day: "Thursday",
+      work: $("#thursday").val(),
+      item: ""
+    },
+    {
+      day: "Friday",
+      work: $("#friday").val(),
+      item: ""
+    },
+    {
+      day: "Saturday",
+      work: $("#saturday").val(),
+      item: ""
+    },
+    {
+      day: "Sunday",
+      work: $("#sunday").val(),
+      item: ""
+    }
+  ]
+shuffle();
+}
+
+function shuffle(){
+  var j, x, i;
+  for (i = toDo.length - 1; i > 0; i--) {
+      j = Math.floor(Math.random() * (i + 1));
+      x = toDo[i];
+      toDo[i] = toDo[j];
+      toDo[j] = x;
+  }
+  createSchedule();
+}
+
+function createSchedule(){
+  for(i=0;i<myWeek.length; i++){
+    for(x=0;x<toDo.length; x++){
+    }
+  }
 }
