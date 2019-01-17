e4695338c68efb7c75ef2e0489e8bb7982dfff5c
smiley-coyote <nick.a.alex@gmail.com>
2019-01-17 10:49:53 -0500

    added files and basic form

---
+++ assets/javascript/app.js
@@ -0,0 +1,63 @@
+var toDo =[
+  {
+    item: "HTML/CSS/JavaScript",
+    selected: false
+  },
+  {
+    item:"Node/SQL",
+    selected: false
+  },
+  {
+    item:"React/Mongo",
+    selected: false
+  },
+  {
+    item:"Software/Mobile Day",
+    selected: false
+  },
+  {
+    item:"Learn something new or catch up on something old",
+    selected: false
+  }
+];
+var myWeek = [
+  {
+    day: "Monday",
+    work: "",
+    item: ""
+  },
+  {
+    day: "Tuesday",
+    work: "",
+    item: ""
+  },
+  {
+    day: "Wednesday",
+    work: "",
+    item: ""
+  },
+  {
+    day: "Thursday",
+    work: "",
+    item: ""
+  },
+  {
+    day: "Friday",
+    work: "",
+    item: ""
+  },
+  {
+    day: "Saturday",
+    work: "",
+    item: ""
+  },
+  {
+    day: "Sunday",
+    work: "",
+    item: ""
+  }
+]
+
+function scheduler(){
+  
+}
