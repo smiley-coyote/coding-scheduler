b0a5de50a5199c124fb50d507a43d1d08385b5e2
smiley-coyote <nick.a.alex@gmail.com>
2019-01-17 14:49:48 -0500

    basics and design are finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,25 +1,31 @@
-var toDo =[
+var toDo = [
   {
-    item: "HTML/CSS/JavaScript"
+    item: "HTML/CSS/JavaScript",
   },
   {
-    item:"Node/SQL"
+    item: "Node/SQL",
   },
   {
-    item:"React/Mongo"
+    item: "React/Mongo",
   },
   {
-    item:"Software/Mobile Day"
+    item: "Software/Mobile Day",
   },
   {
-    item:"Learn something new or catch up on something old"
+    item: "Learn something new",
+  },
+  {
+    item: "You pick",
+  },
+  {
+    item: "Research day",
   }
 ];
 
 var myWeek = [];
 
 
-function scheduler(){
+function scheduler() {
   event.preventDefault();
   myWeek = [
     {
@@ -58,23 +64,56 @@ function scheduler(){
       item: ""
     }
   ]
-shuffle();
+  shuffle();
 }
 
-function shuffle(){
+function shuffle() {
   var j, x, i;
   for (i = toDo.length - 1; i > 0; i--) {
-      j = Math.floor(Math.random() * (i + 1));
-      x = toDo[i];
-      toDo[i] = toDo[j];
-      toDo[j] = x;
+    j = Math.floor(Math.random() * (i + 1));
+    x = toDo[i];
+    toDo[i] = toDo[j];
+    toDo[j] = x;
   }
   createSchedule();
 }
 
-function createSchedule(){
-  for(i=0;i<myWeek.length; i++){
-    for(x=0;x<toDo.length; x++){
+function createSchedule() {
+  for (i = 0; i < myWeek.length; i++) {
+    if (myWeek[i].work === "off") {
+      myWeek[i].item = toDo[i].item + " deep dive and a project"
     }
+    if (myWeek[i].work === "half day") {
+      myWeek[i].item = toDo[i].item + " and project or research"
+    }
+    if (myWeek[i].work === "full day") {
+      myWeek[i].item = toDo[i].item
+    }
+    if (myWeek[i].work === "busy") {
+      myWeek[i].item = toDo[i].item + " casual and quick"
+    }
+
   }
+  runResults();
 }
+
+function runResults() {
+  $("#results").css("display", "block");
+  $("#main").css("display", "none");
+  $("#results").html("<table>")
+  $("#results").append("<th>Day</th>" + "<th>Work</th>" + "<th>Learn</th>")
+  for (i = 0; i < myWeek.length; i++) {
+    $("#results").append("<tr>")
+    $("#results").append("<td>" + myWeek[i].day + "</td>" + "<td>" + myWeek[i].work + "</td>" + "<td>" + myWeek[i].item + "</td>")
+    $("#results").append("</tr>")
+  }
+  $("#results").append("<button onclick='reset()'>Reset</button>");
+  $("#results").append("</table>")
+}
+
+function reset() {
+  event.preventDefault();
+  $("#results").css("display", "none");
+  $("#main").css("display", "block");
+}
+
